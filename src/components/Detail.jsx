5a26f2653144cdd7b41f413b39451bcad6ba8adf
milkabaldi <justmainc8@gmail.com>
2023-01-06 00:17:19 +0200

    Pre-Final changes

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -4,18 +4,18 @@ import images from '../constants/images'
 function Detail() {
   return (
     <div className="detail">
-      <div className="detail__main">
-        <div>
+      <div className="detail__main flex py-32 gap-1 justify-center items-center">
+        <div className='w-6/12'>
           <img src={images.UniCircles} alt="Detail" />
         </div>
-        <div>
-          <h2>Each lesser detail matter</h2>
-          <p>
+        <div className='w-6/12'>
+          <h2 className='text-6xl mb-10 text-dimBlue bold'>Each lesser detail matter</h2>
+          <p className='text-2xl  mb-12 text-dimBlue '>
             Cornelia is an all-in-one tool that helps teams design every part of
             the product experience. Download and get to a functional prototype
             faster than ever.
           </p>
-          <button>Discover Cornelia</button>
+          <button className='transition-colors ease-in-out duration-300 bg-dimWhite hover:bg-dimBlue px-8 py-3 rounded-full text-dimBlue hover:text-dimPeach border-solid border-2 border-dimBlue hover:border-dimPeach'>Discover Cornelia</button>
         </div>
       </div>
     </div>
--- src/components/Unicorns.jsx
+++ src/components/Unicorns.jsx
@@ -4,13 +4,18 @@ import images from '../constants/images'
 function Unicorns() {
   return (
     <section className='appstore'>
-      <div className='appstore__main'>
-        <div className="appstore__main-block">
-          <h2>Unicorns exist</h2>
-          <p>Download our appliaction on the AppStore or the 3D model of Cornelia and meet your new mascot ! Take some cool pictures of her and share it on Instagram, Twitter and Facebook with <b>#CorneliaTheUnicorn</b> !</p>
+      <div className='appstore__main flex py-32 gap-1 justify-center items-center'>
+        <div className="appstore__main-block w-6/12">
+
+          <div className='mb-5 flex justify-center items-center'>
+          <img  src={images.LogoAppStore} alt="App Store" />
+          </div>
+          
+          <h2 className='text-6xl mb-10 text-dimBlue bold'>Unicorns exist</h2>
+          <p className='text-2xl  mb-12 text-dimBlue'>Download our appliaction on the AppStore or the 3D model of Cornelia and meet your new mascot ! Take some cool pictures of her and share it on Instagram, Twitter and Facebook with <b>#CorneliaTheUnicorn</b> !</p>
           <button>Meet Cornelia in VR</button>
         </div>
-        <div className="appstore__main-block">
+        <div className="appstore__main-block w-6/12">
           <img src={images.FrameUni} alt="Frame Unicorns" />
         </div>
       </div>
